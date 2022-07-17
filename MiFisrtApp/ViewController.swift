8ee4ecc98345d024cf72010309cc01d98c2e95a8
Vasili.Baramidze <vasili.baramidze@check24.de>
2022-07-17 19:57:01 +0200

    Initial Commit

---
+++ MiFisrtApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiFisrtApp
+//
+//  Created by Vasili Baramidze on 17.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
